c73747c0f4effe12ff4aa1ad418a81a2ad92a293
Alexey <sologod@yandex.ru>
2021-08-17 16:11:27 +0300

    add buttons

--- app.js
+++ app.js
@@ -6,12 +6,11 @@ const ListCountries = require('./listCountries');
 const bot = new Telegraf(process.env.Bot_Token);
 bot.start((ctx) => ctx.reply(`Привет ${ctx.message.from.first_name}!
 Введите название страны. Посмотреть весь список стран можно командой /help`,
-// Markup.keyboard([
-//   ['Poland', 'Russia'],
-// ])
-//   .resize()
-//   .extra()
-));
+Markup.keyboard([
+  ['Russia', 'Poland', 'Germany', 'Belarus'],
+  ['Kazakhstan', 'Japan', 'Turkey', 'China'],
+])
+  .resize()));
 
 bot.help((ctx) => ctx.reply(ListCountries));
 
@@ -22,9 +21,11 @@ bot.on('text', async (ctx) => {
 
     const formatData = `
 Страна: ${data[0][0].country}
+Флаг: ${data[0][0].flag}
 Случаи: ${data[0][0].cases}
 Смертей: ${data[0][0].deaths}
 Вылечились: ${data[0][0].recovered}
+Aтивные случаи : ${data[0][0].active_cases}
     `;
     ctx.reply(formatData);
   } catch {
