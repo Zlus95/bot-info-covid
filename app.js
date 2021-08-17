9582ce0ca6dbfb98b927e3720604c54c01955c11
Alexey <sologod@yandex.ru>
2021-08-17 13:32:21 +0300

    russia done

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ const api = require('covid19-api');
 const bot = new Telegraf(process.env.Bot_Token);
 bot.start((ctx) => ctx.reply(`Привет ${ctx.message.from.first_name}!`));
 // bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('text', (ctx) => {
+bot.on('text', async (ctx) => {
   let data = {};
-  data = api.getReportsByCountries('russia');
+  data = await api.getReportsByCountries('russia');
+
+  const formatData = `
+Страна: ${data[0][0].country}
+Случаи: ${data[0][0].cases}
+Смертей: ${data[0][0].deaths}
+Вылечились: ${data[0][0].recovered}
+  `;
+  ctx.reply(formatData);
 });
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.launch();
+
+console.log('Бот готов');
