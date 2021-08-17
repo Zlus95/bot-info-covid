6f5386ed078b24084ddbda14c633221ed721b804
Alexey <sologod@yandex.ru>
2021-08-17 15:17:04 +0300

    list countries

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
 require('dotenv').config();
-const { Telegraf } = require('telegraf');
+const { Telegraf, Markup } = require('telegraf');
 const api = require('covid19-api');
+const ListCountries = require('./listCountries');
 
 const bot = new Telegraf(process.env.Bot_Token);
-bot.start((ctx) => ctx.reply(`Привет ${ctx.message.from.first_name}!`));
-// bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.start((ctx) => ctx.reply(`Привет ${ctx.message.from.first_name}!
+Введите название страны. Посмотреть весь список стран можно командой /help`,
+// Markup.keyboard([
+//   ['Poland', 'Russia'],
+// ])
+//   .resize()
+//   .extra()
+));
+
+bot.help((ctx) => ctx.reply(ListCountries));
+
 bot.on('text', async (ctx) => {
   let data = {};
-  data = await api.getReportsByCountries(ctx.message.text);
+  try {
+    data = await api.getReportsByCountries(ctx.message.text);
 
-  const formatData = `
+    const formatData = `
 Страна: ${data[0][0].country}
 Случаи: ${data[0][0].cases}
 Смертей: ${data[0][0].deaths}
 Вылечились: ${data[0][0].recovered}
-  `;
-  ctx.reply(formatData);
+    `;
+    ctx.reply(formatData);
+  } catch {
+    console.log('Ошибка');
+    ctx.reply('Ошибка ввода. Полный список стран можно посмотреть /help');
+  }
 });
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.launch();
 
 console.log('Бот готов');
